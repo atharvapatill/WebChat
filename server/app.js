17fb3dfccb0d178e72953b9d3b7cb3b0059a6359
Atharva Patil <92987731+atharvapatill@users.noreply.github.com>
2026-09-05 23:31:20 +0530

    Update CORS configuration to allow requests from production frontend

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended:true}))
 
 var corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'https://webchat-frontend-p2xu.onrender.com',
   credentials: true 
 }
 app.use(cors(corsOptions));
