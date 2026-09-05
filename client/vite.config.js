dca0cc57a30295241806db880ee1f9008147635f
Atharva Patil <92987731+atharvapatill@users.noreply.github.com>
2026-09-05 23:28:52 +0530

    Add server configuration and format plugins in Vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,5 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: ['webchat-frontend-p2xu.onrender.com'],
+  },
 })
