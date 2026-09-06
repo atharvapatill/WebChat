443c64302277d7403f3e3fab20bd907be7462055
Atharva Patil <92987731+atharvapatill@users.noreply.github.com>
2026-09-06 22:03:55 +0530

    secure cookie

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -58,8 +58,7 @@ const loginUser = async(req,res)=>{
 
         res.cookie("token",token,{
             httpOnly:true,
-            // secure: false,
-            secure:process.env.NODE_ENV === "production",
+            secure: true,
             sameSite: "none",
             maxAge: 24 * 60 * 60 * 1000
         })
